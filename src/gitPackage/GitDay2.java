8ab0e6e18f25ebf594ccc0e5e0a845cfefbde25f
Murat Sinan <muratsinan099@gmail.com>
2020-03-14 20:42:21 +0100

    third commit

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -1,9 +1,17 @@
 package gitPackage;
 
 public class GitDay2 {
-
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello WORLD!");
+		
 
 	}
 
