fa003e8f80e53a5a3981760ca7707b0cad432915
Murat Sinan <muratsinan099@gmail.com>
2020-03-14 21:07:42 +0100

    yeni yorum

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -12,7 +12,7 @@ public class GitDay2 {
 		
 		System.out.println("Hello WORLD!");
 		System.out.println("Hello Git Users!");
-
+		System.out.println("Check if your local is ahead of origin master");
 	}
 
 }
